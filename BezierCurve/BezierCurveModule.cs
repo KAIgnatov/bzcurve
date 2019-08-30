901eae45b12009935d04e5d07216d176ea983227
Константин Александрович Игнатов <ignatov_ka@hotmail.com>
2019-08-30 14:25:52 +1000

    готовый алгоритм. Работает криво - дублируются точки

--- BezierCurve/BezierCurveModule.cs
+++ BezierCurve/BezierCurveModule.cs
@@ -6,6 +6,7 @@ using Topomatic.Sfc;
 using System;
 using Topomatic.Cad.Foundation;
 using Topomatic.Dwg.Layer;
+using Topomatic.Smt;
 
 namespace BezierCurve
 {
@@ -74,28 +75,46 @@ namespace BezierCurve
                 {
                     return;
                 }
+
+                double step = 10.0;
+                GetPointResult gpr_delta = Topomatic.Cad.View.Hints.CadCursors.GetDoubleWithDefault(cadView, ref step, "Укажите укажите размер сегмента после сглаживания, м <10>:");
+                if (gpr_delta != GetPointResult.Accept)
+                {
+                    return;
+                }
+
+                int Nb = 3;
+                GetPointResult gpr_code = Topomatic.Cad.View.Hints.CadCursors.GetIntegerWithDefault(cadView, ref Nb, "Укажите количество сегментов для разбиения коротких участков <3>:");
+                if (gpr_code != GetPointResult.Accept)
+                {
+                    return;
+                }
+
                 var editor = new PointEditor(surface);
 
                 var newline = new Topomatic.Sfc.StructureLine();
+
+                newline.CopyProperty(line);
 
                 SurfacePoint[,] segments = new SurfacePoint[(line.Count - 1), 4];
 
                 var node = line[0];
                 var point = surface.Points[node.Index];
-                segments[0, 0] = point;
+                segments[0, 0] = point.Clone();
 
-                double coef = 0.341;
+
+                double coef = 0.341, raznicaS = 1.0, raznicaF = 1.0;
 
                 for (int i = 0; i < line.Count-2; i++)
                 {
                     double LS = 0, LF = 0;
 
                     var nodeS = line[i];
-                    var pointS = surface.Points[nodeS.Index];
+                    var pointS = surface.Points[nodeS.Index].Clone();
                     var nodeF = line[i+1];
-                    var pointF = surface.Points[nodeF.Index];
+                    var pointF = surface.Points[nodeF.Index].Clone();
                     var nodeN = line[i+2];
-                    var pointN = surface.Points[nodeN.Index];
+                    var pointN = surface.Points[nodeN.Index].Clone();
 
                     LS = Math.Sqrt(Math.Pow(pointF.Vertex.X - pointS.Vertex.X, 2) + Math.Pow(pointF.Vertex.Y - pointS.Vertex.Y, 2) + Math.Pow(pointF.Vertex.Z - pointS.Vertex.Z, 2));
                     LF = Math.Sqrt(Math.Pow(pointN.Vertex.X - pointF.Vertex.X, 2) + Math.Pow(pointN.Vertex.Y - pointF.Vertex.Y, 2) + Math.Pow(pointN.Vertex.Z - pointF.Vertex.Z, 2));
@@ -114,15 +133,28 @@ namespace BezierCurve
                     //}
                     //else { gamma = Math.PI; }
 
-                       gamma = Math.PI + D1 - D2;
+                    gamma = D2 - D1 + 2 * 3.1415926535897932384626433832795;
 
-                    if (gamma < 0)
+
+                    if (gamma < Math.PI )
                     {
-                        gamma = 2 * Math.PI + gamma;
+                        C1 = D2 + ((Math.PI - gamma) / 2) + Math.PI*1.5;
+                        C2 = C1 + Math.PI;
+                    }
+                    else
+                    {
+                        if (D1 > D2)
+                        {
+                            C1 = D2 + ((Math.PI - gamma) / 2) + Math.PI * 1.5;
+                            C2 = C1 + Math.PI;
+                        }
+                        else
+                        {
+                            C1 = D2 - gamma + ((gamma - Math.PI) / 2) + Math.PI * 0.5;
+                            C2 = C1 + Math.PI;
+                        }
                     }
 
-                    C2 = D2 - ((Math.PI - gamma) / 2);
-                    C1 = C2 + Math.PI;
 
                     //if(D1>D2)
                     //{
@@ -131,20 +163,27 @@ namespace BezierCurve
                     //    C1 = temp;
                     //}
 
+                    if (LS / LF > 10)
+                        LS=LF;
 
-                    double dy = LS * coef * Math.Cos(C1);
-                    double dx = LS * coef * Math.Sin(C1);
+                    if (LF / LS > 10)
+                        LF=LS;
+
+
+                    double dy = LS * coef * Math.Cos(C1)*raznicaS;
+                    double dx = LS * coef * Math.Sin(C1)*raznicaS;
 
                     var vectorFC1 = new Vector3D(pointF.Vertex.X + dx, pointF.Vertex.Y + dy, pointF.Vertex.Z);
 
-                    dy = LF * coef * Math.Cos(C2);
-                    dx = LF * coef * Math.Sin(C2);
+                    dy = LF * coef * Math.Cos(C2)*raznicaF;
+                    dx = LF * coef * Math.Sin(C2)*raznicaF;
                     var vectorFC2 = new Vector3D(pointF.Vertex.X + dx, pointF.Vertex.Y + dy, pointF.Vertex.Z);
 
                     SurfacePoint pointFC1 = new SurfacePoint(vectorFC1);
                     SurfacePoint pointFC2 = new SurfacePoint(vectorFC2);
 
-                    segments[i, 3] = segments[i + 1, 0] = pointF;
+                    segments[i, 3] = pointF.Clone();
+                    segments[i + 1, 0] = pointF.Clone();
 
                     if (i == 0)
                     {
@@ -159,13 +198,14 @@ namespace BezierCurve
 
                     if (i == line.Count - 3)
                     {
-                        double C3 = Math.PI + D2 + ((Math.PI - gamma) / 2);
+                        //double C3 = Math.PI + D2 + ((Math.PI - gamma) / 2);
+                        double C3 = C2 + 3.14 + gamma;
                         dy = LF * coef * Math.Cos(C3);
                         dx = LF * coef * Math.Sin(C3);
                         var vectorNC3 = new Vector3D(pointN.Vertex.X + dx, pointN.Vertex.Y + dy, pointN.Vertex.Z);
                         SurfacePoint pointNC3 = new SurfacePoint(vectorNC3);
                         segments[i+1, 2] = pointNC3;
-                        segments[i + 1, 3] = pointN;
+                        segments[i + 1, 3] = pointN.Clone();
                     }
 
                     segments[i, 2] = pointFC1;
@@ -188,7 +228,7 @@ namespace BezierCurve
 
                 try
                 {
-                    for (int i = 0; i < segments.GetLength(0); i++)
+                    /*for (int i = 0; i < segments.GetLength(0); i++)
                     {
                         var cLineS = new Topomatic.Dwg.Entities.DwgPolyline();
                         var cLineF = new Topomatic.Dwg.Entities.DwgPolyline();
@@ -204,7 +244,7 @@ namespace BezierCurve
 
                         drawing.ActiveSpace.Add(cLineS);
                         drawing.ActiveSpace.Add(cLineF);
-                    }
+                    }*/
                 }
                 finally
                 {
@@ -214,7 +254,29 @@ namespace BezierCurve
                 for (int i = 0; i < segments.GetLength(0); i++)
                 {
                     double t = 0.0;
-                    int N = 10;//количество шагов кривой
+                    //int step = 100;
+                    double L1 = Math.Sqrt(Math.Pow(segments[i,3].Vertex.X - segments[i, 0].Vertex.X, 2) + //длина исходного сегмента
+                        Math.Pow(segments[i, 3].Vertex.Y - segments[i, 0].Vertex.Y, 2) + 
+                        Math.Pow(segments[i, 3].Vertex.Z - segments[i, 0].Vertex.Z, 2));
+
+                    double L3 = Math.Sqrt(Math.Pow(segments[i, 1].Vertex.X - segments[i, 0].Vertex.X, 2) +//длина полилинии, проходящей через начальную, две контрольные и конечную точки
+                        Math.Pow(segments[i, 1].Vertex.Y - segments[i, 0].Vertex.Y, 2) +
+                        Math.Pow(segments[i, 1].Vertex.Z - segments[i, 0].Vertex.Z, 2))+
+                        
+                        Math.Sqrt(Math.Pow(segments[i, 2].Vertex.X - segments[i, 1].Vertex.X, 2) +
+                        Math.Pow(segments[i, 2].Vertex.Y - segments[i, 1].Vertex.Y, 2) +
+                        Math.Pow(segments[i, 2].Vertex.Z - segments[i, 1].Vertex.Z, 2))+
+                        
+                        Math.Sqrt(Math.Pow(segments[i, 3].Vertex.X - segments[i, 2].Vertex.X, 2) +
+                        Math.Pow(segments[i, 3].Vertex.Y - segments[i, 2].Vertex.Y, 2) +
+                        Math.Pow(segments[i, 3].Vertex.Z - segments[i, 2].Vertex.Z, 2));
+
+                    double L2 = 1.01 * (L3+L1)/2;
+                    int N = (Int32)Math.Round(L2/step);//количество шагов кривой
+
+                    if (N < Nb)
+                        N = Nb;
+
                     double d = 1.0 / N; //шаг дельта t
                     SurfacePoint[] p = new SurfacePoint[4];
 
@@ -224,7 +286,7 @@ namespace BezierCurve
                     }
 
                     SurfacePoint q = new SurfacePoint();
-                    SurfacePoint r = CastR(p, t, 3, 0);
+                    SurfacePoint r = CastR(p, t, 3, 0).Clone();
 
                     var index = new Topomatic.Sfc.PointEditor(surface).Add(r);
                     newline.Add(index);
@@ -232,16 +294,25 @@ namespace BezierCurve
                     for (int k = 0; k < N; k++)
                     {
                         t += d;
-                        q = CastR(p, t, 3, 0);
-                        r = q;
+                        q = CastR(p, t, 3, 0).Clone();
+                        r = q.Clone();
+                        r.IsExtended = true;
+                        r.IsDynamic = true;
                         index = new Topomatic.Sfc.PointEditor(surface).Add(r);
                         newline.Add(index);
+                        
                     }
                 }
                 var ind = new Topomatic.Sfc.PointEditor(surface).Add(segments[segments.GetLength(0)-1,3]);
                 newline.Add(ind);
 
                 surface.StructureLines.Add(newline);
+
+                for (int l = 0; l < line.Count; l++)
+                {
+                    line.Remove(new StructureLineNode(line[l].Index));
+                }
+                surface.StructureLines.Remove(line);
 
 
             }
@@ -252,7 +323,7 @@ namespace BezierCurve
 
         }
 
-        double DirAngle(SurfacePoint begin, SurfacePoint end)
+       public double DirAngle(SurfacePoint begin, SurfacePoint end)
         {
             double xb, xe, yb, ye, rumb, dy, dx;
 
---
+++ BezierCurve/UnitTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace BezierCurve
+{
+    [TestFixture]
+    class UnitTests
+    {
+        [Test]
+        public static void TestOperations()
+        {
+            BezierCurveModule c = new BezierCurveModule();
+
+            Assert.AreEqual(Math.PI, c.DirAngle(new Topomatic.Sfc.SurfacePoint(new Topomatic.Cad.Foundation.Vector3D(0, 1, 0)), new Topomatic.Sfc.SurfacePoint(new Topomatic.Cad.Foundation.Vector3D(1, 0, 0))));
+        }
+    }
+}
